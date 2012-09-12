15894c5809f22f7ebae4e27473f0d7feefabf649
doug.kaiser@gmail.com <doug.kaiser@gmail.com@cd825799-8ba3-7d5a-edb2-ae3d6b9a22c2>
2012-09-12 04:52:37 +0000

    Created new EmailSvc

---
+++ trunk/src/com/kanner/services/EmailSvc.java
@@ -0,0 +1,43 @@
+package com.kanner.services;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/email")
+public class EmailSvc {
+	
+	@GET
+	public String sendEmail() {
+		
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
+		
+		try {
+			
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("me@me.com", "ME"));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress("you@you.com", "YOU"));
+			
+			message.setSubject("This is a test");
+			
+			message.setText("This is the body of the text");
+			
+			Transport.send(message);
+		} catch (Exception e) {
+			
+			System.out.println("Exception");
+			return "false";
+		}
+		
+		return "true";
+		
+	}
+
+}
